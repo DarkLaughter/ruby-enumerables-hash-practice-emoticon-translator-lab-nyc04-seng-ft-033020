c80be81fbca8484bd0a846abe7524e04a9ca3fcc
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-23 15:54:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,16 +4,15 @@ require "yaml"
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
 
-emo_hash = {
-  :get_meaning => {},
-  :get_emoticon => {}
-}
+    emo_hash = {
+      :get_meaning => {},
+      :get_emoticon => {}
+    }
 
-  emoticons.each do |name, symbols|
-    emo_hash[:get_meaning][symbols[1]] = name
-    emo_hash[:get_emoticon][symbols[0]] = emoticons[name[1]]
-
-end
+    emoticons.each do |name, symbols|
+      emo_hash[:get_meaning][symbols[1]] = name
+      emo_hash[:get_emoticon][symbols[0]] = emoticons[name[1]]
+    end
 
   emo_hash
 end
