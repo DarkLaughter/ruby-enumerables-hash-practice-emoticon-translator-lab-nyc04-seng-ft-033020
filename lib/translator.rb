9f12a696f3ff7fca2ee101bdd8b73481434c69f6
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-23 16:00:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,8 +10,9 @@ def load_library(file_path)
     }
 
     emoticons.each do |name, symbols|
-      emo_hash[:get_meaning][symbols[1]] = name
       emo_hash[:get_emoticon][symbols[0]] = emoticons[name[1]]
+      emo_hash[:get_meaning][symbols[1]] = name
+
     end
 
   emo_hash
