500bf05b63ce7705018477bd75f1fda2372280b2
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-23 17:05:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(file_path, emoticon)
 emoticon_hash = load_library(file_path)
 
   if emoticon_hash[:get_emoticon][emoticon] == nil
-    return "Sorry, not a known emoticon"
+    return "Sorry, that emoticon was not found"
   else
     return emoticon_hash[:get_emoticon][emoticon]
   end
