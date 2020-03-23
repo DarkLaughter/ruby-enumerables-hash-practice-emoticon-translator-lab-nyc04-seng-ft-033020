7c0d023a429d3e665f402b94a588b5a1a363d397
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-23 15:48:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,10 +4,15 @@ require "yaml"
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
 
-new_hash = {
+emo_hash = {
   :get_meaning => {},
   :get_emoticon => {}
 }
+
+  emoticons.each do |name, symbols|
+    emo_hash[:get_meaning][symbols][1] = name 
+    emo_hash[:get_emoticon][symbols][0] = emoticons[:name][1]
+
 end
 
 def get_japanese_emoticon
